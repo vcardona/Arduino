19f9dbf0f37dfbb2bf7a8d9bd5f11e29d52c27a5
Victor Cardona <victorhugocardonagomez@gmail.com>
2019-04-29 13:59:11 -0500

    Add Words Sections
    Se agrego la sección que permite reproducir los Audios grabados anteriormente con cada una de las frases que facilitan la comunicación del usuario.

---
+++ Assets/Scripts/MyMessageListenerAudios.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyMessageListenerAudios : MonoBehaviour 
+{
+    
+    public AudioClip[] AudiosBotones;
+
+    public AudioSource m_MyAudioSource;
+
+    private void Start()
+    {
+        m_MyAudioSource = GetComponent<AudioSource>();
+
+    }
+
+    public void OnMessageArrived(string msg)
+    {
+        Debug.Log("Arrived: " + msg);
+        Debug.Log(msg);
+        CambioColor(msg);
+
+    }
+
+    public void CambioColor(string clave)
+    {
+        Debug.Log("Función Cambio Color");
+        Debug.Log("El mensaje que llego es " + clave);
+
+        int valorEntero = int.Parse(clave);
+
+        switch (valorEntero)
+        {
+            case 1:
+                m_MyAudioSource.clip = AudiosBotones[3];
+                m_MyAudioSource.Play();
+                Debug.Log("Parte Blue del Switch");
+                break;
+            case 2:
+                m_MyAudioSource.clip = AudiosBotones[0];
+                m_MyAudioSource.Play();
+                Debug.Log("Parte Red del Switch");
+                break;
+            case 3:
+                m_MyAudioSource.clip = AudiosBotones[1];
+                m_MyAudioSource.Play();
+                Debug.Log("Parte Green del Switch");
+                break;
+            case 4:
+                m_MyAudioSource.clip = AudiosBotones[2];
+                m_MyAudioSource.Play();
+                Debug.Log("Parte Yellow del Switch");
+                break;
+        }
+    }
+
+    void OnConnectionEvent(bool success)
+    {
+        Debug.Log(success ? "Device connected" : "Device disconnected");
+    }
+}
---
+++ Assets/Scripts/MyMessageListenerSecond.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MyMessageListenerSecond : MonoBehaviour 
+{
+    
+    public string[] nombreEscena;
+
+    private void Start()
+    {
+
+
+    }
+
+    public void OnMessageArrived(string msg)
+    {
+        Debug.Log("Arrived: " + msg);
+        Debug.Log(msg);
+        CambioColor(msg);
+
+    }
+
+    public void CambioColor(string clave)
+    {
+        Debug.Log("Función Cambio Color");
+        Debug.Log("El mensaje que llego es " + clave);
+
+        int valorEntero = int.Parse(clave);
+
+        switch (valorEntero)
+        {
+            case 1:
+                SceneManager.LoadScene(nombreEscena[3]);
+                Debug.Log("Parte Blue del Switch");
+                break;
+            case 2:
+                SceneManager.LoadScene(nombreEscena[0]);
+                Debug.Log("Parte Red del Switch");
+                break;
+            case 3:
+                SceneManager.LoadScene(nombreEscena[1]);
+                Debug.Log("Parte Green del Switch");
+                break;
+            case 4:
+                SceneManager.LoadScene(nombreEscena[2]);
+                Debug.Log("Parte Yellow del Switch");
+                break;
+        }
+    }
+
+    void OnConnectionEvent(bool success)
+    {
+        Debug.Log(success ? "Device connected" : "Device disconnected");
+    }
+}
